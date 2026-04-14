a4de5e5db1c39958849eb0fbc72ca962d4e3f054
RickelviC <Cruzrickelvi@gmail.com>
2026-04-14 11:30:17 -0400

    added user input

--- src/main/java/com/pluralsight/builder.java
+++ src/main/java/com/pluralsight/builder.java
@@ -1,4 +1,39 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class builder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder address = new StringBuilder();
+
+        System.out.println("Please provide the following information: ");
+
+        System.out.print("Full name: ");
+        String fullName = scanner.nextLine();
+
+        System.out.print("Billing Street: ");
+        String billingStreet = scanner.nextLine();
+
+        System.out.print("Billing City: ");
+        String billingCity = scanner.nextLine();
+
+        System.out.print("Billing State: ");
+        String billingState = scanner.nextLine();
+
+        System.out.print("Billing Zip: ");
+        String billingZip = scanner.nextLine();
+
+        System.out.print("Shipping Street: ");
+        String shippingStreet = scanner.nextLine();
+
+        System.out.print("Shipping City: ");
+        String shippingCity = scanner.nextLine();
+
+        System.out.print("Shipping State: ");
+        String shippingState = scanner.nextLine();
+
+        System.out.print("Shipping Zip: ");
+        String shippingZip = scanner.nextLine();
+    }
 }
